c7063ea2b258daa48a9768a84c53b26b8fee952b
DamianR04 <damian.romeijn@hva.nl>
2025-09-25 13:46:24 +0200

    PLACEHOLDER UPDATE

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<section>
+  <img src="/images/bydr.svg" alt="Logo van bydr.studio" width="150" />
+  <h1>Nieuwe website in constructie 🚧</h1>
+  <video src="/images/placeholder.mp4" autoplay loop muted></video>
+</section>
+
+<style>
+section{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  img{
+    position: absolute;
+    top: 2rem;
+    z-index: 1;
+  }
+  h1{
+    position: absolute;
+    color: white;
+    z-index: 1;
+    font-size: 5vw;
+  }
+
+  video{
+    filter: blur(2px) ;
+    width: 100dvw;
+    height: 100dvh;
+    object-fit: cover;
+    z-index: -1;
+  }
+</style>
