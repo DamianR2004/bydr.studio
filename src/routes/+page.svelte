0667306223257026e1090c951435753f2b433553
DamianR04 <damian.romeijn@hva.nl>
2026-02-16 16:52:38 +0100

    update placeholder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<section>
+  <h1>Nieuwe website is onderweg!</h1>
+  <h2>bydr<span>.</span>studio</h2>
+</section>
+
+<video src="/bckg.mp4" autoplay muted loop></video>
+
+<style>
+  section{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  video{
+    max-width: 100vw;
+    height: auto;
+    object-fit: cover;
+    position: relative;
+    z-index: -1;
+  }
+
+  h1, h2{
+    font-size: 3rem;
+    color: white;
+    position: absolute;
+  }
+
+  span{
+    color: orange;
+  }
+</style>
