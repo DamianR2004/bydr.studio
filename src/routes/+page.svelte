1531bd15ceb2c490db132bb4c71bb0ac61aead24
DamianR04 <damian.romeijn@hva.nl>
2026-04-06 16:57:30 +0200

    quickfix font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <section>
     <p>Nieuwe website is onderweg!</p>
-    <img src="/bydrstudio.svg" alt="Logo van bydr.studio">
     <h1>bydr<span>.</span>studio</h1>
     <h2>Testrun</h2>
 </section>
@@ -13,18 +12,5 @@
     flex-direction: column;
     height: 100vh;
     background-color: var(--color-bg);
-  }
-
-  h1{
-    font-size: 5.5vw;
-    color: var(--color-text);
-  }
-
-  img{
-    height: 50vh;
-  }
-
-  span{
-    color: orange;
   }
 </style>
