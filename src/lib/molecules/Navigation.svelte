e385256ecd3ab7040523d392787352abee91e11b
DamianR04 <damian.romeijn@hva.nl>
2026-04-08 19:45:44 +0200

    update v0.1

---
+++ src/lib/molecules/Navigation.svelte
@@ -0,0 +1,17 @@
+<nav>
+  <ul>
+    <li><a href="/"><img src="" alt="logo van bydr.studio"></a></li>
+    <li><a href="/projecten">Projecten</a></li>
+    <li><a href="/about">Over ons</a></li>
+    <li><a href="/contact">Contact</a></li>
+  </ul>
+</nav>
+
+<nav>
+  <ul>
+    <li><a href="/"><img src="" alt="logo van bydr.studio"></a></li>
+    <li><a href="/projecten">Projecten</a></li>
+    <li><a href="/about">Over ons</a></li>
+    <li><a href="/contact">Contact</a></li>
+  </ul>
+</nav>
