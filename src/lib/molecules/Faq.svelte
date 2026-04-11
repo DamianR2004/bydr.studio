6a733bb98e63270128b46b773161802edd5ac24a
Damian <144007686+DamianR2004@users.noreply.github.com>
2026-04-11 21:25:11 +0200

    Create Faq.svelte

---
+++ src/lib/molecules/Faq.svelte
@@ -0,0 +1,29 @@
+<section>
+    <h2>Veelgestelde vragen</h2>
+        <ul>
+            <li>
+                <details>
+                    <summary>Wat zijn de kosten van een website?</summary>
+                    <p></p>
+                </details>
+            </li>
+            <li>
+                <details>
+                    <summary>Wat onderscheidt jou van anderen?</summary>
+                    <p></p>
+                </details>
+            </li>   
+            <li>
+                <details>
+                    <summary>Kan ik later eigen content aan de site toevoegen?</summary>
+                    <p></p>
+                </details>
+            </li>
+            <li>
+                <details>
+                    <summary>Werkt mijn website ook goed op mobiel?</summary>
+                    <p></p>
+                </details>
+            </li>
+        </ul>
+</section>
