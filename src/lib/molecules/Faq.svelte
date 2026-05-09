f37a0977c433c0d6e251ffa25f1edb6ec65b0bff
Damian <144007686+DamianR2004@users.noreply.github.com>
2026-05-09 13:55:34 +0200

    update 2

--- src/lib/molecules/Faq.svelte
+++ src/lib/molecules/Faq.svelte
@@ -27,3 +27,49 @@
             </li>
         </ul>
 </section>
+
+<style>
+
+li{
+    list-style: none;
+}
+
+details {
+  border-block-end: 1px solid var(--color-secondary);
+  margin-block: .5rem;
+  padding-block: .5rem;
+}
+
+summary {
+  position: relative;
+  anchor-name: --summary;
+  
+  &::marker {
+    content: "";
+  }
+  
+  &::before,
+  &::after {
+    content: "";
+    border-block-start: 3px solid var(--color-secondary);
+    height: 0;
+    width: 1rem;
+    
+    inset-block-start: 50%;
+    inset-inline-end: 0;
+    
+    position: absolute;
+    position-anchor: --summary;
+    position-area: top end;
+  }
+  
+  &::after {
+    transform: rotate(90deg);
+    transform-origin: 50%;
+  }
+}
+
+details[open] summary::after {
+  transform: rotate(0deg);
+}
+</style>
