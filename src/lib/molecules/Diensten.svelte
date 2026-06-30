59033c5e3c1c55f92f477a321d3899ab586b6e2d
Damian <144007686+DamianR2004@users.noreply.github.com>
2026-06-30 02:13:09 +0200

    Update Diensten.svelte

--- src/lib/molecules/Diensten.svelte
+++ src/lib/molecules/Diensten.svelte
@@ -3,37 +3,28 @@
   <ul>
     <li><h3>Webdesign</h3></li>
     <li><p></p></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
+    <li>UI/UX Design</li>
+    <li>Responsive Design (Mobiel & Tablet)</li>
+    <li>Interaction Design</li>
+    <li>Redesign van bestaande websites</li>
    </ul>
 
 <ul>
     <li><h3>Webdevelopment</h3></li>
     <li><p></p></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
+    <li>Maatwerk Websites</li>
+    <li>CMS Implementatie</li>
+    <li>Website Optimalisatie (Snelheid & SEO)</li>
+    <li>Onderhoud, Support & Hosting</li>
+
 </ul>
 
 <ul>
     <li><h3>Branding</h3></li>
     <li><p></p></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
+    <li>Huisstijl & Visuele Identiteit</li>
+    <li>Brand Identity Guidelines</li>
+    <li>Typografie & Kleurpaletten</li>
 </ul>
 </section>
 
